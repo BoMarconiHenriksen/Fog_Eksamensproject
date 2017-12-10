ba995864fdea35947d070cbac03e7691ec95d48f
BenedikteEva <cph-bm109@cphbusiness.dk>
2017-12-10 18:15:07 +0100

    Test af Calculator

---
+++ Fog_Eksamensprojekt01/src/test/java/Business/CalculatorTest.java
@@ -0,0 +1,154 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Ejer
+ */
+public class CalculatorTest {
+    
+    public CalculatorTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of totalPriceSimpleCarport method, of class Calculator.
+     */
+    //@Test 
+    public void testTotalPriceSimpleCarport() throws Exception {
+        System.out.println("totalPriceSimpleCarport");
+        double length = 240.0;
+        double width = 240.0;
+        double heigth = 225.0;
+        double shedLength = 0.0;
+        double shedWidth = 0.0;
+        Calculator instance = new Calculator();
+        double expResult = 2790.60;
+        double result = instance.totalPriceSimpleCarport(length, width, heigth, shedLength, shedWidth);
+        assertEquals(expResult, result, 0.0);
+        // This one failed because there are things in the shed that are constant so it shows that we
+        // have to keep method for calculating shed price and method for calculating the carport apart
+        
+
+    }
+
+    /**
+     * Test of calculateCarportSimple method, of class Calculator.
+     */
+    @Test
+    public void testCalculateCarportSimple() throws Exception {
+        System.out.println("calculateCarportSimple");
+        double length = 240.0;
+        double width = 240.0;
+        double heigth = 225.0;
+        Calculator instance = new Calculator();
+        double expResult = 2790.60;
+        double result = instance.calculateCarportSimple(length, width, heigth);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+       
+    }
+    
+ 
+    /**
+     * Test of numberOfRafters method, of class Calculator.
+     */
+    @Test
+    public void testNumberOfRafters() {
+        System.out.println("numberOfRafters");
+        double length = 480.0;
+        int expResult = 8;
+        int result = Calculator.numberOfRafters(length);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+      
+    }
+
+    /**
+     * Test of spaceBetweenRafters method, of class Calculator.
+     */
+    @Test
+    public void testSpaceBetweenRafters() {
+        System.out.println("spaceBetweenRafters");
+        double length = 480.0;
+        boolean expResult=true;
+      boolean result=false;
+        int space = Calculator.spaceBetweenRafters(length);
+        if (space >=54&&space<=60){
+           result=true;
+        }
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+   
+    }
+
+    /**
+     * Test of numberOfBottomScrewsPackageEcolite method, of class Calculator.
+     */
+    @Test
+    public void testNumberOfBottomScrewsPackageEcolite() {
+        System.out.println("numberOfBottomScrewsPackageEcolite");
+        double length = 600.0;
+        double width = 300.0;
+        int expResult = 2;
+        int result = Calculator.numberOfBottomScrewsPackageEcolite(length, width);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+     
+    }
+
+    /**
+     * Test of calculatePriceShed method, of class Calculator.
+     */
+    @Test
+    public void testCalculatePriceShed() throws Exception {
+        System.out.println("calculatePriceShed");
+        double shedLength = 210.0;
+        double shedWidth = 150.0;
+        Calculator instance = new Calculator();
+        double expResult = 2778.48;
+        double result = instance.calculatePriceShed(shedLength, shedWidth);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+     
+    }
+
+    /**
+     * Test of main method, of class Calculator.
+     */
+    @Test
+    public void testMain() throws Exception {
+        System.out.println("main");
+        String[] args = null;
+        Calculator.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+      
+    }
+    
+}
